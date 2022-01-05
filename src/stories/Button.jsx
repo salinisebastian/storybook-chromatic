75b2fde8da998de546756b2d1e8c95e65610c3aa
Salini Sebastian <60488691+salinisebastian@users.noreply.github.com>
2022-01-05 12:38:30 +0100

    Update Button.jsx

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label} Test1234567
+      {label} 
     </button>
   );
 };
